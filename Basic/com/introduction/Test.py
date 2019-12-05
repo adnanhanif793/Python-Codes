563dfa39085ecb189af4fc1ccfd552ffda8f9d96
adnanhanif793 <adnanhanif793@gmail.com>
2019-12-05 15:25:46 +0530

    commit new

---
+++ Basic/com/introduction/Test.py
@@ -0,0 +1,14 @@
+'''
+Created on 13-Jun-2019
+
+@author: adnan.hanif
+'''
+
+print("{:<10}".format("rat"), end="")
+print(258)
+print("{:*>6}".format("rat")) 
+print("{:5d}".format(12))   
+
+s="Adnan"
+l=list(s)
+print(l)
